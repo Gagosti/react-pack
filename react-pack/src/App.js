5ee53ae3dc1b5833d8bf87062e4954168f10e779
Guilherme Santos <agustinho_santos@hotmail.com>
2022-07-03 22:33:41 -0300

    set counter

--- react-pack/src/App.js
+++ react-pack/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from './Header'
+import Counter from './Counter'
 
 function App() {
   return (
+    <div>
     <Header name="ProgBr" links = {["About", "Buy", "Contact"]}></Header>
+    <Counter count={1}></Counter>
+    </div>
   );
 }
 
