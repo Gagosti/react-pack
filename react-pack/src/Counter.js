d1730b654648b1eefae70bebb349b93d585d709e
Guilherme Santos <agustinho_santos@hotmail.com>
2022-07-11 13:04:36 -0300

    state with class

--- react-pack/src/Counter.js
+++ react-pack/src/Counter.js
@@ -5,14 +5,19 @@ class Counter extends React.Component {
   constructor(props){
     super(props);
 
-    this.state = { count: this.props.count }
+    this.state = { count: 0 }
     this.add = this.add.bind(this)
   }
 
   add() {
     //this.setState({ count: this.state.count + 1 })
     this.setState((state) => {return {count: state.count + 1}})
+    localStorage.setItem("state", JSON.stringify(this.state))
   }
+
+componentDidMount () {
+  this.setState(JSON.parse(localStorage.getItem('state')))
+}
 
   render() {
     return (<div>
